e855edd9af1ed506dc7ea5fb7c199f7e67ae2a53
seanjcarley <seanjcarley@outlook.com>
2020-02-19 20:23:28 +0000

    added templates html files base, index, contact, careers, about, and updated run.py

--- flask/run.py
+++ flask/run.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")  # aka "view"
 def index():
-    return "Hello, world!"
+    # return "<h1>Hello,</h1><h2>world!</h2>"
+    return render_template("index.html")
+
+
+@app.route("/about")  # aka "view"
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")  # aka "view"
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")  # aka "view"
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
