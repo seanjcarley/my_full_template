d9ce401511b3d9f539a702d43b593fb7e587e502
seanjcarley <seanjcarley@outlook.com>
2020-02-25 20:56:50 +0000

    updated run.py to show member.html, added member.html, added url to company.json, and update about.html

--- flask/run.py
+++ flask/run.py
@@ -16,7 +16,7 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company = data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")  # aka "view"
@@ -27,6 +27,17 @@ def contact():
 @app.route("/careers")  # aka "view"
 def careers():
     return render_template("careers.html", page_title="Careers")
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 if __name__ == "__main__":
